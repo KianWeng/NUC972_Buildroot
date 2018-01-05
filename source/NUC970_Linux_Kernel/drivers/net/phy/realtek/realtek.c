61d034668d4b5e9540a8b5b01de0737a9cd32a81
wenggaojian <wenggj@kongtrolink.com>
2018-01-05 17:26:56 +0800

    [NUC972][CHG]修改RTL8309M交换机驱动，为用户空间提供读写寄存器的接口.

--- source/NUC970_Linux_Kernel/drivers/net/phy/realtek/realtek.c
+++ source/NUC970_Linux_Kernel/drivers/net/phy/realtek/realtek.c
@@ -15,8 +15,15 @@
  */
 #include <linux/phy.h>
 #include <linux/module.h>
-#include <linux/gpio.h>
-#include <linux/delay.h>
+#include <linux/cdev.h>
+#include <asm/io.h>
+#include <asm/system.h>
+#include <asm/uaccess.h>
+#include <linux/mm.h>
+#include <linux/types.h>
+#include <linux/fs.h>
+#include <linux/device.h>
+
 
 #define RTL821x_PHYSR		0x11
 #define RTL821x_PHYSR_DUPLEX	0x2000
@@ -27,9 +34,36 @@
 
 #define	RTL8211E_INER_LINK_STAT	0x10
 
+#define RTL8309_MAGIC 'R'
+#define RTL8309M_GET_REG_DATA _IO(RTL8309_MAGIC, 0x20)
+#define RTL8309M_SET_REG_DATA _IO(RTL8309_MAGIC, 0x21)
+#define RTL8309M_READ_REG _IO(RTL8309_MAGIC, 0x22)
+#define RTL8309M_WRITE_REG _IO(RTL8309_MAGIC, 0x23)
+
+
 MODULE_DESCRIPTION("Realtek PHY driver");
 MODULE_AUTHOR("Johnson Leung");
 MODULE_LICENSE("GPL");
+
+struct rtl8309m_register_data {
+	unsigned int phyad;
+	unsigned int regad;
+};
+
+struct rtl8309m_dev {
+	struct cdev cdev;
+	struct rtl8309m_register_data register_data;
+	struct phy_device *phydev;
+};
+
+static int rtl8309m_major = 250;
+static int rtl8309m_minor = 0;
+static dev_t devno;
+static struct class *rtl8309m_class;
+static struct device *rtl8309m_device;
+static struct rtl8309m_dev *rtl8309m_devp;
+
+
 
 static int rtl821x_ack_interrupt(struct phy_device *phydev)
 {
@@ -92,6 +126,7 @@ static int rtl8309m_config_init(struct phy_device *phydev)
 	phydev->autoneg = AUTONEG_ENABLE;
 
 	phydev->state = PHY_RUNNING;
+	rtl8309m_devp->phydev = phydev;
 	
     //printk(KERN_INFO"Marvell:return config init.\n");	
 	return 0;
@@ -126,6 +161,83 @@ static int rtl8309m_read_status(struct phy_device *phydev)
 	return 0;
 }
 
+int smiWrite(struct phy_device *phydev, struct rtl8309m_register_data *register_data, unsigned int value){
+	int err;
+	struct phy_device tmp;
+
+	tmp = *phydev;
+	tmp.addr = register_data->phyad;
+
+	err = phy_write(&tmp, register_data->regad, value);
+
+	return (err < 0) ? err : 0;
+}
+
+int smiRead(struct phy_device *phydev, struct rtl8309m_register_data *register_data){
+	int ret;
+	struct phy_device tmp;
+
+	tmp = *phydev;
+	tmp.addr = register_data->phyad;
+
+	ret= phy_read(&tmp, register_data->regad);
+
+	return ret;
+}
+
+/* Open the device */
+static int rtl8309m_open( struct inode *inode, struct file *filp ){
+	struct rtl8309m_dev *dev;
+	/* 获得设备结构体指针 */
+	dev = container_of(inode->i_cdev, struct rtl8309m_dev, cdev);
+	/* 让设备结构体作为设备的私有信息 */
+	filp->private_data = dev;
+    return 0;
+}
+
+
+
+/* Close hello_device */
+static int rtl8309m_release( struct inode *inode, struct file *filp ){
+    return 0;
+}
+
+/* ioctl函数 */
+static long rtl8309m_ioctl(struct file *filp, unsigned int cmd, unsigned long arg){
+
+	int value;
+	struct rtl8309m_dev *dev = filp->private_data;
+
+	//printk(KERN_INFO"Realtek:cmd is 0x%x,get is 0x%x,set is 0x%x,read is 0x%x,write is 0x%x.\n",cmd,RTL8309M_GET_REG_DATA,RTL8309M_SET_REG_DATA,RTL8309M_READ_REG,RTL8309M_WRITE_REG);
+	switch(cmd){
+		case RTL8309M_GET_REG_DATA:
+			//printk(KERN_INFO"Realtek:ioctl get reg data!\n");
+			if(copy_to_user((struct rtl8309m_register_data *)arg, &dev->register_data, sizeof(struct rtl8309m_register_data)))
+				return -EFAULT;
+			break;
+		case RTL8309M_SET_REG_DATA:
+			//printk(KERN_INFO"Realtek:ioctl set reg data!\n");
+			if(copy_from_user(&dev->register_data, (struct rtl8309m_register_data *)arg, sizeof(struct rtl8309m_register_data)))
+				return -EFAULT;
+			break;
+		case RTL8309M_READ_REG:
+			//printk(KERN_INFO"Realtek:ioctl read reg!\n");
+			value = smiRead(dev->phydev, &dev->register_data);
+			if(copy_to_user((int *)arg, &value, sizeof(int)))
+				return -EFAULT;
+			break;
+		case RTL8309M_WRITE_REG:
+			//printk(KERN_INFO"Realtek:ioctl write reg!\n");
+			if(copy_from_user(&value, (int *)arg, sizeof(int)))
+				return -EFAULT;
+			smiWrite(dev->phydev, &dev->register_data, value);
+			break;
+		default:
+			break;
+	}
+
+	return 0;
+}
 
 /* RTL8211B */
 static struct phy_driver rtl8211b_driver = {
@@ -172,6 +284,23 @@ static struct phy_driver rtl8309m_driver = {
 	.driver		= { .owner = THIS_MODULE,},
 };
 
+static struct file_operations rtl8309m_fops = {
+	.owner = THIS_MODULE,
+	.open = rtl8309m_open,
+	.release = rtl8309m_release,
+	.unlocked_ioctl = rtl8309m_ioctl,
+};
+
+static void rtl8309m_setup_cdev(struct rtl8309m_dev *dev){
+	int err;
+    /* initialize the cdev struct */
+    cdev_init(&dev->cdev, &rtl8309m_fops);
+    dev->cdev.owner = THIS_MODULE;
+	dev->cdev.ops = &rtl8309m_fops;
+    err = cdev_add(&dev->cdev, devno, 1); /* register the cdev in the kernel */
+    if(err)
+        printk(KERN_ERR"Error %d adding rtl8309 cdev.\n", err);
+}
 
 static int __init realtek_init(void)
 {
@@ -179,24 +308,53 @@ static int __init realtek_init(void)
 
 	printk(KERN_INFO"Realtek:start to init.\n");
 
-	/*
-	ret = gpio_request(NUC970_PB5, "rtl8309m_reset");
-	if(ret < 0)
-		printk(KERN_ERR"Realtek: requset reset pin fail.\n");
+	devno = MKDEV(rtl8309m_major, rtl8309m_minor);
+	ret = register_chrdev(rtl8309m_major,"rtl8309m",&rtl8309m_fops);
 
-	gpio_direction_output(NUC970_PB5,1);
-	mdelay(20);
-	gpio_direction_output(NUC970_PB5,0);
-	*/
-	
+	rtl8309m_devp = kmalloc(sizeof(struct rtl8309m_dev), GFP_KERNEL);
+	if(!rtl8309m_devp){
+		ret = -ENOMEM;
+		goto fail_malloc;
+	}
+	memset(rtl8309m_devp, 0, sizeof(struct rtl8309m_dev));
+	rtl8309m_setup_cdev(rtl8309m_devp);
+
+	rtl8309m_class = class_create(THIS_MODULE, "rtl8309m");
+	if(IS_ERR(rtl8309m_class)){
+		ret = -EBUSY;
+		goto fail_create_class;
+	}
+	rtl8309m_device = device_create(rtl8309m_class,NULL,devno,NULL,"rtl8309m"); 
+    if(IS_ERR(rtl8309m_device))  {  
+        ret = -EBUSY;
+		goto fail_create_device;
+    }     
+
 	ret = phy_driver_register(&rtl8309m_driver);
-	if (ret < 0)
-		return -ENODEV;
+	if (ret < 0){
+		ret = -ENODEV;
+		goto fail_register_phy;
+	}
+	
+	return ret;
+
+fail_register_phy:	
+	device_destroy(rtl8309m_class,devno); 
+fail_create_device:
+	class_destroy(rtl8309m_class);  
+fail_create_class:
+	kfree(rtl8309m_devp);
+fail_malloc:
+	unregister_chrdev(rtl8309m_major,"rtl8309m"); 
+	
 	return ret;
 }
 
 static void __exit realtek_exit(void)
 {
+	device_destroy(rtl8309m_class,devno);  
+    class_destroy(rtl8309m_class);   
+    unregister_chrdev(rtl8309m_major,"rtl8309m"); 
 	phy_driver_unregister(&rtl8309m_driver);
 }
 

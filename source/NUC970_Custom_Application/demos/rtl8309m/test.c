9ed2e612f43ffaafab9b57d83d757569e7773407
wenggaojian <wenggj@kongtrolink.com>
2018-08-06 10:59:43 +0800

    [NUC972][ADD]添加缺失文件.

--- source/NUC970_Custom_Application/demos/rtl8309m/test.c
+++ source/NUC970_Custom_Application/demos/rtl8309m/test.c
@@ -24,28 +24,55 @@ int main(void)
     int ret;
 	rtk_portmask_t mbrmsk1, mbrmsk2, untagmsk1, untagmsk2;
 	rtk_fid_t fid1,fid2;
+    rtk_port_phy_data_t data;
 
-	//³õÊ¼»¯½»»»»ú
+	//ï¿½ï¿½Ê¼ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
     ret = rtk_switch_init();
 	if(ret < 0){
 		printf("rtk switch init fail,just exit!");
 		exit(0);
 	}
 
-	//³õÊ¼»¯½»»»vlan¹¦ÄÜ
+	//ï¿½ï¿½Ê¼ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½vlanï¿½ï¿½ï¿½ï¿½
 	rtk_vlan_init();
-
-	mbrmsk1.bits[0] = 0x143;
-	untagmsk1.bits[0] = 0x1FF;
-	fid1 = 1;
-	rtk_vlan_set(1, mbrmsk1, untagmsk1, fid1);
-
-	mbrmsk2.bits[0] = 0x1C;
-	untagmsk2.bits[0] = 0x1FF;
-	fid2 = 1;
-	rtk_vlan_set(2, mbrmsk2, untagmsk2, fid2);
+	rtk_vlan_destroy(1);
 	
-	//ÊÍ·Å½»»»»ú×ÊÔ´
+	mbrmsk1.bits[0] = 0x15E;
+	untagmsk1.bits[0] = 0x15E;
+	fid1 = 0;
+	rtk_vlan_set(100, mbrmsk1, untagmsk1, fid1);
+
+	mbrmsk2.bits[0] = 0x120;
+	untagmsk2.bits[0] = 0x120;
+	fid2 = 0;
+	rtk_vlan_set(101, mbrmsk2, untagmsk2, fid2);
+	
+	mbrmsk2.bits[0] = 0x1FF;
+	untagmsk2.bits[0] = 0x1FF;
+	fid2 = 0;
+	rtk_vlan_set(102, mbrmsk2, untagmsk2, fid2);
+	
+	rtk_vlan_portPvid_set(1,100,0);
+	rtk_vlan_portPvid_set(2,100,0);
+	rtk_vlan_portPvid_set(3,100,0);
+	rtk_vlan_portPvid_set(4,100,0);
+	rtk_vlan_portPvid_set(6,100,0);
+	rtk_vlan_portPvid_set(5,101,0);
+	rtk_vlan_portPvid_set(8,102,0);
+	
+    rtk_port_phyReg_get(1,1,&data);
+    printf("port 1 status is 0x%x.\n",data);
+	rtk_port_phyReg_get(2,1,&data);
+    printf("port 2 status is 0x%x.\n",data);
+	rtk_port_phyReg_get(3,1,&data);
+    printf("port 3 status is 0x%x.\n",data);
+	rtk_port_phyReg_get(4,1,&data);
+    printf("port 4 status is 0x%x.\n",data);
+	rtk_port_phyReg_get(5,1,&data);
+    printf("port 5 status is 0x%x.\n",data);
+	rtk_port_phyReg_get(6,1,&data);
+    printf("port 6 status is 0x%x.\n",data);
+	//ï¿½Í·Å½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ô´
 	rtk_switch_release();
 	
     return 0;
